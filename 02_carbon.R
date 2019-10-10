24ebf218ce82bbf627fc444637a8b2138c486f1b
clittlef <clittlef@uw.edu>
2019-10-10 11:00:38 -0700

    CRITICAL: changed factor GEOID to numeric else counties selected based on factor index. Expanded VT example to CONUS

--- 02_carbon.R
+++ 02_carbon.R
@@ -5,79 +5,83 @@ par(mfrow=c(1,1))
 
 # Load all datasets
 county <- st_read(paste0(county.dir, "CONUS.county.p.shp"))
+# MUST convert GEOID to numeric else factor index get used
+county$GEOID <- as.numeric(as.character(county$GEOID))
 c2015 <- raster(paste0(c.dir, "c2015.p.tif")) ; names(c2015) <- "c2015"
 nlcd <- raster(paste0(lulc.dir, "nlcd.reclass.960.tif")) ; names(nlcd) <- "nlcd"
 r.county <- fasterize(county, nlcd, field = "GEOID"); names(r.county) <- "county"
 plot(r.county) # Appears to be by state, but intra-state values differ.
-stack <- stack(c2015, nlcd, r.county)
-names(stack)
 
-## Do test landscape
+
+###########################################
+###########################################
+#### VT test landscape ####################
+###########################################
+###########################################
+
+## Set shapefiles and stack resters to use
 vt <- county %>% filter(STATEFP == "50")
+windsor <- vt %>% filter(GEOID == 50027)
+franklin <- vt %>% filter(GEOID == 50011)
 r.vt <- fasterize(vt, nlcd, field = "GEOID") %>% crop(vt) %>% mask(vt) ; names(r.vt) <- "vt"
 c2015.test <- c2015 %>% crop(vt) %>% mask(vt)
 nlcd.test <- nlcd %>% crop(vt) %>% mask(vt)
 stack.test <- stack(c2015.test, nlcd.test, r.vt)
 names(stack.test) <- c("c2015.test", "nlcd.test", "vt")
 
-ctemp <- list() 
+## Looping thru counties to summarize carbon values by cover type.
+# N.b., some counties don't have all zones; keep zonal output as matrix to match zones in rbind
+l.vt <- list() 
 loop.ready <- unique(values(r.vt)) # Grab unique county IDs
 loop.ready <- loop.ready[is.finite(loop.ready)] # Retain numbers, nix NA
+# loop.ready <- 50011
 for(i in loop.ready) {
-  mask <- r.vt %in% i # works vs. filtering or with r.vt==i
+  mask <- r.vt %in% i # Filtering or r.vt==i don't work
   mask[mask == 0] <- NA # remove NA areas
-  stack.test.2 <- stack.test %>% mask(mask) # extract carbon & lu vals
-  byzone <- zonal(stack.test.2$c2015.test, stack.test.2$nlcd.test, fun = "mean", digits = 0)
-  # Not all zones are in every county; keep coefficients as matrices (vs. byzone[,2])
-  # Transpose to put zones as column names for use with rbind.fill.matrix()
-  byzone <- data.frame(t(byzone))
-  
-  ## START HERE. NOTE ZONE NAMES (NUMBERS) ARE RETAINED IN ZONAL. SEE 13th COUNTY IN VT
-  
-  colnames(byzone) <- paste0("z",byzone[1,])
-  byzone <- byzone %>% dplyr::select()
-  rownames(byzone) <- paste0("c",loop.ready)
-  ctemp[[paste0(i)]] <- byzone 
+  target <- stack.test %>% mask(mask) # Only keep those in mask; crop messes with extents so don't use.
+  byzone <- zonal(target$c2015.test, target$nlcd.test, fun = "mean", digits = 0) # Avg. c val per class
+  byzone <- data.frame(t(byzone)) # df else sets to vector when dropping row below; t() to set zones as col names
+  colnames(byzone) <- paste0("z",byzone[1,]) # skip rownames -- get dropped in rbind.fillmatrix()
+  byzone <- byzone[-c(1),] # Get rid of zone row
+  l.vt[[paste0(i)]] <- byzone # Fill in list
 }
 
-https://stackoverflow.com/questions/45374422/rename-column-of-dataframes-inside-a-list-with-its-dataframe-name
-z <- lapply(ctemp, data.frame)
-z <- map_dfr(ctemp)
-z <- lapply(z, fun(x) {colnames(x) <- paste0("z", as.integer(x[1,])); x})
-z[[13]]
-test <- plyr::rbind.fill.matrix(ctemp)
-rownames(test) <- paste0("county",loop.ready)
-?plyr::rbind.fill.matrix
-
-ctemp[[1]]
-ctemp[[2]]
-ctemp[[11]]
-ctemp[[13]]
+# Bind all dataframes of carbon by class for each county.
+m.vt <- plyr::rbind.fill.matrix(l.vt) # Preserves column names (here, zones defined in loop)
+rownames(m.vt) <- paste0("c",loop.ready) # Assign names based on county IDs. 
+head(m.vt, 10)
 
 
-cs <- data.frame(cs)
-rownames(cs) <- loop.ready
-colnames(cs) <- 
-class(ctemp[[1]])
-test <- do.call(rbind(ctemp))
-test <- bind
-ctemp[[1]]
-  }
-plot(stack.test.2[[1]])
-plot(mask)
+###########################################
+###########################################
+#### All of CONUS #########################
+###########################################
+###########################################
 
-i <- 2776
-names(stemp)
-plot(stemp$c2015)
-plot(stemp$nlcd)
-getValues(stemp$nlcd)
-hist(stemp$nlcd)
-plot(temp[[1]])
-plot(temp[[2]])
-  }
-  i
-  
+## Looping thru counties to summarize carbon values by cover type.
+# N.b., some counties don't have all zones; keep zonal output as matrix to match zones in rbind
 
-  
-# Extract (by polygon)
-# Zonal (by NLCD raster)
+stack <- stack(c2015, nlcd, r.county)
+names(stack)
+
+start <- Sys.time()
+l <- list() 
+loop.ready <- unique(values(r.county)) # Grab unique county IDs
+loop.ready <- loop.ready[is.finite(loop.ready)] # Retain numbers, nix NA
+for(i in loop.ready) {
+  mask <- r.county %in% i # Filtering or r.vt==i don't work
+  mask[mask == 0] <- NA # remove NA areas
+  target <- stack.test %>% mask(mask) # Only keep those in mask; crop messes with extents so don't use.
+  byzone <- zonal(target$c2015, target$nlcd, fun = "mean", digits = 0) # Avg. c val per class
+  byzone <- data.frame(t(byzone)) # df else sets to vector when dropping row below; t() to set zones as col names
+  colnames(byzone) <- paste0("z",byzone[1,]) # skip rownames -- get dropped in rbind.fillmatrix()
+  byzone <- byzone[-c(1),] # Get rid of zone row
+  l.vt[[paste0(i)]] <- byzone # Fill in list
+}
+Sys.time() - start
+
+# Bind all dataframes of carbon by class for each county.
+m <- plyr::rbind.fill.matrix(l) # Preserves column names (here, zones defined in loop)
+rownames(m) <- paste0("c",loop.ready) # Assign names based on county IDs. 
+head(m, 10)
+
